7e6be487c869d5a3b0d7939bab3d1c60222a08a9
Andy Pilate <cubox@cubox.me>
2014-04-27 21:23:05 +0200

    Configuration file support, big fixes and curl support

---
+++ README.md
@@ -0,0 +1,27 @@
+# Pasteyourmom
+
+Simple paste service.
+
+Demo here: [http://paste.cubox.me]
+
+## How to install?
+
+You need [Go](http://golang.org) installed.
+
+```
+mkdir paste
+cd paste
+go get github.com/Cubox-/pasteyourmom
+wget https://raw.githubusercontent.com/Cubox-/pasteyourmom/master/index.html
+wget https://raw.githubusercontent.com/Cubox-/pasteyourmom/master/style.css
+wget https://raw.githubusercontent.com/Cubox-/pasteyourmom/master/config.json
+vim config.json
+pasteyourmom -bind :(your port here) -conf config.json
+```
+
+Those steps are here for the example, modify following your needs.
+You can put a nginx or apache in front of this, use the XRealIP configuration variable.
+
+## License
+
+MIT.
--- pasteyourmom.go
+++ pasteyourmom.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+    "encoding/json"
     "flag"
     "io"
+    "io/ioutil"
     "log"
     "math/rand"
     "net/http"
@@ -15,41 +17,76 @@ import (
     "github.com/zenazn/goji/web/middleware"
 )
 
-const (
-    idSet      string = "abcdefjhijklmnopqrstuvwxyzABCDEFJHIJKLMNOPQRSTUVWXYZ1234567890"
-    idLength   int    = 5
-    dataFolder string = "./"
-)
+type configuration struct {
+    DataFolder  string
+    FormTitle   string
+    IdLength    int
+    IdSet       string
+    StaticFiles []string
+    RealIP      bool
+}
 
 var (
+    conf configuration = configuration{
+        "./",
+        "content",
+        5,
+        "abcdefjhijklmnopqrstuvwxyzABCDEFJHIJKLMNOPQRSTUVWXYZ1234567890",
+        []string{"index.html", "style.css"},
+        false}
     stdoutLogger *log.Logger
-    staticFiles  []string = []string{"index.html", "style.css"}
 )
 
+func dumpDefaultConf() {
+    buf, err := json.Marshal(conf)
+    if err != nil {
+        stdoutLogger.Fatal(err)
+    }
+
+    os.Stdout.Write(buf)
+    os.Stdout.Write([]byte("\n"))
+}
+
+func setConf(confFile string) {
+    content, err := ioutil.ReadFile(confFile)
+    if err != nil {
+        stdoutLogger.Fatal(err)
+    }
+
+    err = json.Unmarshal(content, &conf)
+    if err != nil {
+        stdoutLogger.Fatal(err)
+    }
+}
+
 func genId() string {
-    length := idLength
+    length := conf.IdLength
     endstr := make([]byte, length)
+
     i := 0
+
     for ; length > 0; length-- {
-        endstr[i] = idSet[rand.Intn(len(idSet)-1)]
+        endstr[i] = conf.IdSet[rand.Intn(len(conf.IdSet)-1)]
         i++
     }
+
     return string(endstr)
 }
 
 func isStaticFile(file string) bool {
-    for _, element := range staticFiles {
+    for _, element := range conf.StaticFiles {
         if file == element {
             return true
         }
     }
+
     return false
 }
 
 func root(c web.C, w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "text/html")
 
-    file, err := os.Open(dataFolder + "index.html")
+    file, err := os.Open(conf.DataFolder + "index.html")
     if err != nil {
         http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
         stdoutLogger.Print(err)
@@ -59,6 +96,7 @@ func root(c web.C, w http.ResponseWriter, r *http.Request) {
     _, err = io.Copy(w, file)
     if err != nil {
         http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+        stdoutLogger.Print(err)
         return
     }
 }
@@ -68,7 +106,8 @@ func getPaste(c web.C, w http.ResponseWriter, r *http.Request) {
         getStatic(c.URLParams["id"], w)
         return
     }
-    file, err := os.Open(dataFolder + c.URLParams["id"] + ".paste")
+
+    file, err := os.Open(conf.DataFolder + c.URLParams["id"] + ".paste")
     if os.IsNotExist(err) {
         http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
         return
@@ -82,16 +121,18 @@ func getPaste(c web.C, w http.ResponseWriter, r *http.Request) {
     _, err = io.Copy(w, file)
     if err != nil {
         http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+        stdoutLogger.Print(err)
         return
     }
 }
 
 func createPaste(w http.ResponseWriter, r *http.Request) {
     id := genId()
-    file, err := os.Create(dataFolder + id + ".paste")
+
+    file, err := os.Create(conf.DataFolder + id + ".paste")
     for os.IsExist(err) {
         id = genId()
-        file, err = os.Create(dataFolder + id + ".paste")
+        file, err = os.Create(conf.DataFolder + id + ".paste")
     }
     if err != nil {
         http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
@@ -99,7 +140,7 @@ func createPaste(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    text := r.FormValue("text")
+    text := r.FormValue(conf.FormTitle)
     if text == "" {
         http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
         return
@@ -108,10 +149,18 @@ func createPaste(w http.ResponseWriter, r *http.Request) {
     n, err := file.Write([]byte(text))
     if err != nil || n != len(text) {
         http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+        stdoutLogger.Print(err)
         return
     }
 
     http.Redirect(w, r, id, http.StatusSeeOther)
+
+    n, err = w.Write([]byte(r.Host + "/" + id + "\n"))
+    if err != nil {
+        http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+        stdoutLogger.Print(err)
+        return
+    }
 }
 
 func getStatic(filename string, w http.ResponseWriter) {
@@ -119,7 +168,8 @@ func getStatic(filename string, w http.ResponseWriter) {
     if len(filenameSplitted) > 1 {
         w.Header().Set("Content-Type", "text/"+filenameSplitted[len(filenameSplitted)-1])
     }
-    file, err := os.Open(dataFolder + filename)
+
+    file, err := os.Open(conf.DataFolder + filename)
     if os.IsNotExist(err) {
         http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
         return
@@ -129,21 +179,40 @@ func getStatic(filename string, w http.ResponseWriter) {
         stdoutLogger.Print(err)
         return
     }
+
     _, err = io.Copy(w, file)
     if err != nil {
         http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+        stdoutLogger.Print(err)
         return
     }
 }
 
 func main() {
-    xRealIP := flag.Bool("realip", false, "use X-Real-IP for getting the IP adress of the client")
+    var dumpConf bool
+    var confFile string
+
+    flag.BoolVar(&dumpConf, "dumpconf", false, "dump the default configuration file")
+    flag.StringVar(&confFile, "conf", "", "use this alternative configuration file")
     flag.Parse()
+
     stdoutLogger = log.New(os.Stderr, "", log.Flags())
+
+    if dumpConf {
+        dumpDefaultConf()
+        return
+    }
+
     rand.Seed(time.Now().Unix())
-    if *xRealIP {
+
+    if confFile != "" {
+        setConf(confFile)
+    }
+
+    if conf.RealIP {
         goji.Insert(middleware.RealIP, middleware.Logger)
     }
+
     goji.Get("/", root)
     goji.Get("/:id", getPaste)
     goji.Post("/", createPaste)
